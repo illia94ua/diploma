e99fd9da01f2ac0ac20a3f9ff78a00f1a3443868
Illia Chernous <illyachernous@mail.ru>
2016-06-14 00:52:40 +0300

    changes

--- src/main/java/test/test/Canny.java
+++ src/main/java/test/test/Canny.java
@@ -16,31 +16,36 @@ import org.opencv.imgproc.Imgproc;
 public class Canny {
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Mat src = Highgui.imread("4.jpg");
+		Mat src = Highgui.imread("2.jpg");
 		Mat gaus = new Mat();
 		Mat gray = new Mat();
-		Mat canny = new Mat();
+//		Mat canny = new Mat();
 		Mat thr = new Mat();
 
 
 		Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
 		Imgproc.GaussianBlur(gray, gaus, new Size(3, 3), 4);
 
-//		Imgproc.threshold(gaus, thr, 137, 255, Imgproc.THRESH_BINARY_INV);//1.png
-//		Imgproc.threshold(gaus, thr, 10, 255, Imgproc.THRESH_BINARY_INV);//2.jpg
-		Imgproc.threshold(gaus, thr, 120, 255, Imgproc.THRESH_BINARY);//4.jpg
+//		Imgproc.threshold(gaus, thr, 139, 255, Imgproc.THRESH_BINARY_INV);//1.png
+		Imgproc.threshold(gaus, thr, 10, 255, Imgproc.THRESH_BINARY_INV);//2.jpg
+//		Imgproc.threshold(gaus, thr, 120, 255, Imgproc.THRESH_BINARY);//4.jpg
+//		Imgproc.threshold(gaus, thr, 120, 255, Imgproc.THRESH_BINARY_INV);//5.jpg
+//		Imgproc.threshold(gaus, thr, 113, 255, Imgproc.THRESH_BINARY);//6.jpg
 		Highgui.imwrite("thr.jpg", thr);
-		Imgproc.Canny(thr, canny, 30, 150);
-		Highgui.imwrite("canny.jpg", canny);
-
+//		Imgproc.Canny(thr, canny, 30, 150);
+//		Highgui.imwrite("canny.jpg", canny);
+		
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		Imgproc.findContours(canny, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+		Imgproc.findContours(thr, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 		
 //		Imgproc.drawContours(src, contours, -1, new Scalar(0, 0, 255));
 		for(int i=0; i< contours.size();i++){
-	        if (Imgproc.contourArea(contours.get(i)) > 100 /*&& Imgproc.contourArea(contours.get(i)) < 25*/){
+			double area = Imgproc.contourArea(contours.get(i));
+//	        if (area > 200 && area < 7600){//5.jpg
+	        if (area > 200 && area < 3400){//2.jpg
+//	        if (area > 3500 && area < 23000){//6.jpg
 	            Rect rect = Imgproc.boundingRect(contours.get(i));
-	            	Core.rectangle(src, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,0,255));
+	            	Core.rectangle(src, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,0,255), 2);
 	        }
 	    }
 		Highgui.imwrite("cont.jpg", src);
